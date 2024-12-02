6bb3dcafd97f8ea729b28f23e9a2169a794743cb
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-12-02 16:41:21 -0500

    created variables and changed contentProjectDiv to match new class name

--- src/project-creator.js
+++ src/project-creator.js
@@ -1,2 +1,13 @@
-const contentProjectDiv = document.querySelector("contentSidebarDiv");
-const contentTodoDiv = document.querySelector("contentTodoDiv");
+const contentProjectDiv = document.querySelector("contentProjectDiv");
+let projectArray = []
+
+//Declaring HTML Variables
+const divCreate = document.createElement("div");
+const h1Create = document.createElement("h1");
+const pCreate = document.createElement("p");
+
+
+//Functions
+function createProject(){
+
+}

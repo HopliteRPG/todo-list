1ceb3c1506ff6ca918942eb33b92e087757652af
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-12-02 16:51:45 -0500

    created createP,createH1 functions

--- src/project-creator.js
+++ src/project-creator.js
@@ -8,6 +8,20 @@ const pCreate = document.createElement("p");
 
 
 //Functions
-function createProject(){
+function createP(div,p,text,className){
+    let clonePCreate = p.cloneNode(true);
+    clonePCreate.innerText = text;
+    clonePCreate.classList.add(className);
+    div.appendChild(clonePCreate);
+}
+
+function createH1(div,h1,text,className){
+    let cloneH1Create = h1.cloneNode(true);
+    cloneH1Create.innerText = text;
+    cloneH1Create.classList.add(className);
+    div.appendChild(cloneH1Create);
+}
+
+function createProject(parentDiv,childDiv,h1,p){
 
 }

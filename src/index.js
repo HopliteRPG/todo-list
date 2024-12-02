2ca7a67117576a764ec9414106b3ee70df434fb9
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-12-02 17:22:42 -0500

    testing adding projects to contentProjectDiv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./styles.css";
+import { createProject } from "./project-creator";
 
-
+createProject();
--- src/project-creator.js
+++ src/project-creator.js
@@ -1,27 +1,44 @@
-const contentProjectDiv = document.querySelector("contentProjectDiv");
-let projectArray = []
+export{createProject}
 
-//Declaring HTML Variables
-const divCreate = document.createElement("div");
-const h1Create = document.createElement("h1");
-const pCreate = document.createElement("p");
+function createProject(){
+    //Declaring HTML Variables
+    const divCreate = document.createElement("div");
+    const h1Create = document.createElement("h1");
+    const pCreate = document.createElement("p");
+    const contentProjectDiv = document.querySelector(".contentProjectDiv");
+    let projectArray = []
 
 
-//Functions
-function createP(div,p,text,className){
-    let clonePCreate = p.cloneNode(true);
-    clonePCreate.innerText = text;
-    clonePCreate.classList.add(className);
-    div.appendChild(clonePCreate);
-}
+    //Functions
+    function createDivSection(parentDiv,div,className){
+        let cloneDivCreate = div.cloneNode(true);
+        cloneDivCreate.classList.add(className)
+        parentDiv.appendChild(cloneDivCreate);
+        return{cloneDivCreate}
+    }
 
-function createH1(div,h1,text,className){
-    let cloneH1Create = h1.cloneNode(true);
-    cloneH1Create.innerText = text;
-    cloneH1Create.classList.add(className);
-    div.appendChild(cloneH1Create);
-}
+    function createP(div,p,text,className){
+        let clonePCreate = p.cloneNode(true);
+        clonePCreate.innerText = text;
+        clonePCreate.classList.add(className);
+        div.appendChild(clonePCreate);
+    }
 
-function createProject(parentDiv,childDiv,h1,p){
+    function createH1(div,h1,text,className){
+        let cloneH1Create = h1.cloneNode(true);
+        cloneH1Create.innerText = text;
+        cloneH1Create.classList.add(className);
+        div.appendChild(cloneH1Create);
+    }
 
+    function appendProject(parentDiv,childDiv,h1,p,className){
+        parentDiv.appendChild(childDiv);
+        childDiv.className.add(className);
+        childDiv.appendChild(h1);
+        childDiv.appendChild(p);
+    }
+
+    const testDiv = createDivSection(contentProjectDiv,divCreate,"testDiv");
+    const testH1 = createH1(testDiv.cloneDivCreate,h1Create,"Test H1","testH1");
+    const testP = createP(testDiv.cloneDivCreate,pCreate,"this is a test description","testP")
 }

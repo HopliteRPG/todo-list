d1afb9ad782e1889b99e98d5ad027d7fee5fc809
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-12-12 23:34:40 -0500

    added checkboxes to todos

--- src/todo-creator.js
+++ src/todo-creator.js
@@ -7,13 +7,15 @@ function todoCreator(){
     const h1Create = document.createElement("h1");
     const pCreate = document.createElement("p");
     const contentTodoDiv = document.querySelector(".contentTodoDiv");
+    const todoCheckbox = document.createElement("INPUT");
+    todoCheckbox.setAttribute("type", "checkbox");
 
     //Functions
     function createDivSection(parentDiv,div,className){
         let cloneDivCreate = div.cloneNode(true);
         cloneDivCreate.classList.add(className)
         parentDiv.appendChild(cloneDivCreate);
-        return{cloneDivCreate}
+        return{cloneDivCreate};
     }
 
     function createP(div,p,text,className){
@@ -23,10 +25,17 @@ function todoCreator(){
         div.appendChild(clonePCreate);
     }
 
+    function createTodoCheckbox(div,checkbox,className){
+        let cloneTodoCheckbox = checkbox.cloneNode(true);
+        cloneTodoCheckbox.classList.add(className);
+        div.appendChild(cloneTodoCheckbox);
+        return{cloneTodoCheckbox};
+    }
+
     function createTodoObject(id){
         let todoObjectId = id;
         let todoContentArray = [];
-        return{id,todoContentArray}
+        return{id,todoContentArray};
     }
     
     function createTodo(todoObject,desc){
@@ -47,7 +56,8 @@ function todoCreator(){
     function appendTodo(todoObject){
         todoObject.todoContentArray.forEach(element => {
             let todoDiv = createDivSection(contentTodoDiv,divCreate,"todoDiv");
-            createP(todoDiv.cloneDivCreate,pCreate,element,"projectP");
+            createTodoCheckbox(todoDiv.cloneDivCreate,todoCheckbox,"todoCheckbox")
+            createP(todoDiv.cloneDivCreate,pCreate,element,"todoP");
         });
     }
 

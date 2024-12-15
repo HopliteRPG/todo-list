c8cd17a7f1d4d5f53019c377002820465246e4fa
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-12-14 21:20:16 -0500

    fixed touchHTML not working by putting it underneath appendProject

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { appendProject } from "./project-creator-html";
 
 const contentProjectDiv = document.querySelector(".contentProjectDiv");
 
+
 createProject("Test H1","This is a test to see if P works");
 createProject("Bruh H1","lorem ipsum");
 createProject("HUH H1","HSJLDAFH;LSDJK");
 createProject("lol H1","lol lol lol");
 
-touchHTML(contentProjectDiv,".projectDiv");
 
 appendProject(projectArray)
+
+touchHTML(contentProjectDiv,".projectDiv");
